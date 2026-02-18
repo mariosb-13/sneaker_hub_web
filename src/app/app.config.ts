03851e6a693bd47e7cc4e65cfe114396d51cb8cb
mariosb-13 <msanbar104g@iescarrillo.es>
2026-02-18 13:19:49 +0100

    Integrate Firebase and update project config
    Prepare project for Firebase integration: add an empty firebase.json, update .gitignore to ignore Firebase files and debug logs, rename the Angular project id in angular.json, add @angular/fire to package.json (and regenerate package-lock), and update src/app/app.config.ts with Firebase-related configuration. These changes set up the codebase for connecting to Firebase and clean up generated/runtime files.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "sneakerhub-3862d", appId: "1:161400644046:web:cde4ab955fb4b66ba10b9f", databaseURL: "https://sneakerhub-3862d-default-rtdb.firebaseio.com", storageBucket: "sneakerhub-3862d.firebasestorage.app", apiKey: "AIzaSyBy_YOW6m7lMKM4Rb4hRnrVkHO8A7oFIHk", authDomain: "sneakerhub-3862d.firebaseapp.com", messagingSenderId: "161400644046", measurementId: "G-YGX8D3GP8E", projectNumber: "161400644046", version: "2" })), provideAuth(() => getAuth()), provideDatabase(() => getDatabase())]
 };
